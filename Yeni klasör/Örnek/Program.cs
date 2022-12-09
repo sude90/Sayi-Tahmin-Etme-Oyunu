ba5e8f80b1ac5a375f1551fb589b2f87a7d7b8bf
Sude Naz <78275279+sude90@users.noreply.github.com>
2022-12-09 12:27:13 +0300

    Add files via upload
    Oyun bir sayı tahmin etme oyunudur ve dizilerden yararlanılarak oluşturulmuştur. Toplam 10 tane rastgele sayı oluşturuluyor ve 2 oyuncu sırası ile bu sayıları tahmin ediyor. En sonunda iki oyuncunun da toplam puanları ekrana yazdırılıyor. 
      ekran ilk açıldığında birinci oyuncunun tahmini ve ardından da ikinci oyuncunun tahmininin girilmesi gerekmektedir. Bu sıra 10 soru bitene kadar devam eder.

---
+++ Yeni klasör/Örnek/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Örnek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            Random r = new Random();
+            int[] rastgele_sayi = new int[3];
+            for (int i = 0; i < rastgele_sayi.Length; i++)
+            {
+                rastgele_sayi[i] = r.Next(1,10);
+            }
+            int ilk_puan = 0;
+            int ikinci_puan = 0;
+            for (int i = 0; i < rastgele_sayi.Length; i++)
+            {
+                Console.WriteLine("Birinci oyuncu tahmini : ");
+                int tahmini_sayi1 = Convert.ToInt32(Console.ReadLine());
+                if (tahmini_sayi1 != rastgele_sayi[i])
+                {
+                    Console.WriteLine("Yanlış Tahmin");
+
+                    if (rastgele_sayi[i] > tahmini_sayi1)
+                    {
+                        ilk_puan += 10 - (rastgele_sayi[i] - tahmini_sayi1);
+                    }
+                    else if (rastgele_sayi[i] != tahmini_sayi1)
+                    {
+                        ilk_puan += 10 - (tahmini_sayi1 - rastgele_sayi[i]);
+                    }
+                }
+                else if (tahmini_sayi1 == rastgele_sayi[i])
+                {
+                    Console.WriteLine("Doğru Tahmin");
+                }
+                Console.Clear();
+
+                //*******************************************//
+
+                Console.WriteLine("İkinci oyuncu tahmini : ");
+                int tahmini_sayi2 = Convert.ToInt32(Console.ReadLine());
+                if (tahmini_sayi2 != rastgele_sayi[i])
+                {
+                    Console.WriteLine("Yanlış Tahmin");
+                    if (rastgele_sayi[i] > tahmini_sayi2)
+                    {
+                        ikinci_puan += 10 - (rastgele_sayi[i] - tahmini_sayi2);
+                    }
+                    else if (rastgele_sayi[i] != tahmini_sayi2)
+                    {
+                        ikinci_puan += 10 - (tahmini_sayi2 - rastgele_sayi[i]);
+                    }
+                }
+                else if (tahmini_sayi2 == rastgele_sayi[i])
+                {
+                    Console.WriteLine("Doğru Tahmin");
+                }
+            }
+
+            Console.Clear();
+            Console.WriteLine("1.Oyuncunun Puanı : " + ilk_puan);
+            Console.WriteLine("2.Oyuncunun Puanı : " + ikinci_puan);
+            Console.ReadKey();
+        }
+    }
+}
